262ae6dd6f81f7019678c09c1ed2452618b576b7
Jack Murray <jack@murray.software>
2023-01-01 11:35:56 +0000

    fix game.players bug and add height to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
-from db import Base
+from sqlalchemy.orm import Session
+from db import Base, Height
 from env import DB
+from bitcoin import get_height
 
 if __name__ == '__main__':
   engine = create_engine(DB)
   Base.metadata.create_all(engine)
+  with Session(engine) as session:
+    session.add(Height(height=get_height()))
+    session.commit()
