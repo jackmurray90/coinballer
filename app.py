fc8055863d5673d4adfed82140e5d6e3b13afa32
Jack Murray <jack@murray.software>
2023-01-02 08:47:54 +0000

    remove use_cases

--- app.py
+++ app.py
@@ -28,11 +28,6 @@ def about():
   rate_limit()
   return render_template('about.html')
 
-@app.route('/use_cases')
-def use_cases():
-  rate_limit()
-  return render_template('use_cases.html')
-
 @app.route('/new_game', methods=['GET', 'POST'])
 def new_game():
   rate_limit()
